c4261631c7f65fbd1d9c2d1b6dbde9d67e75cb40
Felizardo Caculo <felizardo1987@gmail.com>
2022-05-18 21:00:44 +0100

    Adicionando o servidor e os arquivos estáticos

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + '/assets/index.html')
+});
+
+app.use(router);
+app.use(express.static(`${__dirname}/assets`));
+
+app.listen(process.env.PORT || 5500, () => {
+    console.log('SERVIDOR RODANDO NA PORTA 5500')
+});
